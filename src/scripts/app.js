9c8c7399e3a0ffc5b94dc39ce0a40912418bf92f
Marija Cehic <marijac@stuntcoders.com>
2022-02-18 11:22:31 +0100

    Add image filtering to Filter section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -23,3 +23,54 @@
     });
 
 })(jQuery);
+
+// Filter images
+(function () {
+    const $galleryImgs = $('#gallery img');
+    const $filterButtons = $('.filter__buttons');
+    const taggedImage = {};
+
+    $galleryImgs.each(function () {
+        const img = this;
+        const tags = $(this).data('tags');
+
+        if (tags) {
+            tags.split(',').forEach(function (tagName) {
+                if (taggedImage[tagName] == null) {
+                    taggedImage[tagName] = [];
+                }
+                taggedImage[tagName].push(img);
+            })
+        }
+    })
+
+    $('<button/>', {
+        text: 'Show All',
+        class: 'filter__button filter__button--active',
+        click: function () {
+            $(this)
+                .addClass('filter__button--active')
+                .siblings()
+                .removeClass('filter__button--active');
+            $galleryImgs.show();
+        }
+    }).appendTo($filterButtons);
+
+    $.each(taggedImage, function (tagName) {
+        const $n = $(taggedImage[tagName]).length;
+        $('<button/>', {
+            text: tagName,
+            class: 'filter__button',
+            click: function () {
+                $(this)
+                    .addClass('filter__button--active')
+                    .siblings()
+                    .removeClass('filter__button--active');
+                $galleryImgs
+                    .hide()
+                    .filter(taggedImage[tagName])
+                    .show();
+            }
+        }).appendTo($filterButtons);
+    });
+}())
