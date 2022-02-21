c75205a2d9a893212a435cbc50e09e0fcc34e04e
Marija Cehic <marijac@stuntcoders.com>
2022-02-21 16:00:52 +0100

    Add webpack to allow require

--- gulpfile.js
+++ gulpfile.js
@@ -9,58 +9,73 @@ const terser = require("gulp-terser");
 const babel = require("gulp-babel");
 const autoprefixer = require("autoprefixer");
 const browsersync = require("browser-sync").create();
+const webpack = require("webpack-stream");
 
 //CSS tasks
 function cssTask(cb) {
-  src("./src/sass/styles.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(
-      rename({
-        extname: ".min.css",
-      })
-    )
-    .pipe(gulp.dest("./dist/css"));
-  cb();
+	src("./src/sass/styles.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./dist/css"))
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(
+			rename({
+				extname: ".min.css",
+			})
+		)
+		.pipe(gulp.dest("./dist/css"));
+	cb();
 }
 
 //Javascript tasks
 function babelTask(cb) {
-  src("src/scripts/**/*.js")
-    .pipe(
-      babel({
-        presets: ["@babel/preset-env"],
-      })
-    )
-    .pipe(terser())
-    .pipe(dest("dist/scripts"));
-    cb();
+	src("src/scripts/**/*.js")
+		.pipe(
+			babel({
+				presets: ["@babel/preset-env"],
+			})
+		)
+		.pipe(terser())
+		.pipe(dest("dist/scripts"));
+	cb();
+}
+
+function webpackTask() {
+	return gulp
+		.src("src/scripts/app.js")
+		.pipe(webpack())
+		.pipe(gulp.dest("dist/scripts"));
 }
 
 //Browser tasks
 function browsersyncServer(cb) {
-  browsersync.init({
-    server: {
-      baseDir: ".",
-    },
-  });
-  cb();
+	browsersync.init({
+		server: {
+			baseDir: ".",
+		},
+	});
+	cb();
 }
 
 //Reload on save
 function browsersyncReload(cb) {
-  browsersync.reload();
-  cb();
+	browsersync.reload();
+	cb();
 }
 
 // Watch Task
 function watchTask() {
-  watch(["*html", "src/html/**/*.html"], browsersyncReload);
-  watch(["src/sass/**/*.scss", "src/scripts/**/*.js"],
-    series(cssTask, babelTask, browsersyncReload)
-  );
+	watch(["*html", "src/html/**/*.html"], browsersyncReload);
+	watch(
+		["src/sass/**/*.scss", "src/scripts/**/*.js"],
+		series(cssTask, babelTask, browsersyncReload, webpackTask)
+	);
 }
 
 // Default Gulp Task
-exports.default = series(cssTask, browsersyncServer, watchTask, babelTask);
+exports.default = series(
+	cssTask,
+	browsersyncServer,
+	watchTask,
+	babelTask,
+	webpackTask
+);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,76 +1,88 @@
+import $ from "jquery";
+import slick from "slick-carousel";
+
 (function () {
+	const searchIcon = $(".nav__search__icon");
+	const searchInput = $(".nav__search__input");
+	const closeButton = $(".nav__search__input__close");
+	const menuIcon = $(".nav__menu__icon");
 
-    const searchIcon = $(".nav__search__icon");
-    const searchInput = $('.nav__search__input');
-    const closeButton = $('.nav__search__input__close');
-    const menuIcon = $(".nav__menu__icon");
+	$(menuIcon).on("click", function () {
+		menuIcon.nextAll().slideToggle("slow");
+	});
 
-    $(menuIcon).on("click", function () {
-        menuIcon.nextAll().slideToggle("slow");
-    });
+	$(searchIcon).on("click", function () {
+		$(searchIcon).slideToggle("slow");
+		$(searchInput).slideToggle("slow");
+		$(closeButton).slideToggle("slow");
+	});
 
-    $(searchIcon).on("click", function () {
-        $(searchIcon).slideToggle("slow");
-        $(searchInput).slideToggle("slow");
-        $(closeButton).slideToggle("slow");
-    });
-
-    $(closeButton).on("click", function () {
-        $(searchIcon).slideToggle("slow");
-        $(searchInput).slideToggle("slow");
-        $(closeButton).slideToggle("slow");
-
-    });
-
+	$(closeButton).on("click", function () {
+		$(searchIcon).slideToggle("slow");
+		$(searchInput).slideToggle("slow");
+		$(closeButton).slideToggle("slow");
+	});
 })(jQuery);
 
 // Filter images
 (function () {
-    const $galleryImgs = $('#filter__gallery img');
-    const $filterButtons = $('.filter__buttons');
-    const taggedImage = {};
+	const $galleryImgs = $("#filter__gallery img");
+	const $filterButtons = $(".filter__buttons");
+	const taggedImage = {};
 
-    $galleryImgs.each(function () {
-        const img = this;
-        const tags = $(this).data('tags');
+	$galleryImgs.each(function () {
+		const img = this;
+		const tags = $(this).data("tags");
 
-        if (tags) {
-            tags.split(',').forEach(function (tagName) {
-                if (taggedImage[tagName] == null) {
-                    taggedImage[tagName] = [];
-                }
-                taggedImage[tagName].push(img);
-            })
-        }
-    })
+		if (tags) {
+			tags.split(",").forEach(function (tagName) {
+				if (taggedImage[tagName] == null) {
+					taggedImage[tagName] = [];
+				}
+				taggedImage[tagName].push(img);
+			});
+		}
+	});
 
-    $('<button/>', {
-        text: 'Show All',
-        class: 'filter__button filter__button--active',
-        click: function () {
-            $(this)
-                .addClass('filter__button--active')
-                .siblings()
-                .removeClass('filter__button--active');
-            $galleryImgs.show();
-        }
-    }).appendTo($filterButtons);
+	$("<button/>", {
+		text: "Show All",
+		class: "filter__button filter__button--active",
+		click: function () {
+			$(this)
+				.addClass("filter__button--active")
+				.siblings()
+				.removeClass("filter__button--active");
+			$galleryImgs.show();
+		},
+	}).appendTo($filterButtons);
 
-    $.each(taggedImage, function (tagName) {
-        const $n = $(taggedImage[tagName]).length;
-        $('<button/>', {
-            text: tagName,
-            class: 'filter__button',
-            click: function () {
-                $(this)
-                    .addClass('filter__button--active')
-                    .siblings()
-                    .removeClass('filter__button--active');
-                $galleryImgs
-                    .hide()
-                    .filter(taggedImage[tagName])
-                    .show();
-            }
-        }).appendTo($filterButtons);
-    });
-}())
+	$.each(taggedImage, function (tagName) {
+		const $n = $(taggedImage[tagName]).length;
+		$("<button/>", {
+			text: tagName,
+			class: "filter__button",
+			click: function () {
+				$(this)
+					.addClass("filter__button--active")
+					.siblings()
+					.removeClass("filter__button--active");
+				$galleryImgs.hide().filter(taggedImage[tagName]).show();
+			},
+		}).appendTo($filterButtons);
+	});
+})();
+
+// Team Slick Carousel
+(function () {
+	const slickWrapper = $(".slick-slider-wrapper");
+
+	$(slickWrapper).slick({
+		infinite: true,
+		speed: 600,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		autoplay: true,
+	});
+})(jQuery);
