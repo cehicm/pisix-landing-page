67fb2e7e6796ccc056e43c4915cec65320425358
Marija Cehic <marijac@stuntcoders.com>
2022-02-22 11:23:10 +0100

    Add newsletter button functionallity & styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -124,3 +124,43 @@ import slick from "slick-carousel";
 		autoplay: true,
 	});
 })(jQuery);
+
+//Newsletter subscribe
+(function () {
+	$("#subscribe-btn").on("click", function () {
+		let $this = $(this),
+			$inputMessage = $this.siblings(".input-message"),
+			$inputEmail = $this.siblings(".input-email"),
+			$wrapper = $this.closest(".newsletter__subscribe");
+
+		const errorMessages = {
+			emptyField: "This field can't be empty!",
+			invalidEmail: "Please enter a valid email",
+			success: "Thank you for subscribing!",
+		};
+
+		const emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		let isValid = $inputEmail.val().match(emailRegex);
+
+		if ($inputEmail.val() === "") {
+			$inputMessage.remove();
+			$inputEmail.addClass("input-border-error");
+			$wrapper.append(
+				`<p class="input-message error"> ${errorMessages.emptyField}</p>`
+			);
+		} else if ($inputEmail.val() === "" || isValid === null) {
+			$inputMessage.remove();
+			$inputEmail.addClass("input-border-error");
+			$wrapper.append(
+				`<p class="input-message error"> ${errorMessages.invalidEmail}</p>`
+			);
+		} else {
+			$inputMessage.remove();
+			$this.addClass("input-btn--clicked");
+			$inputEmail.removeClass("input-border-error");
+			$wrapper.append(
+				`<p class="input-message success"> ${errorMessages.success}</p>`
+			);
+		}
+	});
+})(jQuery);
